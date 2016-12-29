3e16188c49549e3b9053970d912c642db966cfeb
yirolhao <yirolhao@gmail.com>
2016-12-29 22:18:00 +0800

    裁剪上传头像

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { in: 6..20 } , allow_nil: true	
 
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, 
+	has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }, 
 																	default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/								
 
