f40e334d5ce98fc310a4e6ecff16955bd3328d89
yirolhao <yirolhao@gmail.com>
2017-01-19 19:20:53 +0800

    test comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
                                    dependent:   :destroy
 	has_many :followers, through: :passive_relationships, source: :follower
 
+	has_many :comments , dependent: :destroy
+
 	validates :name, presence: true, length: { maximum: 20 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@comment = articles(:login_user).comments.new(
+									user_id: users(:login_user).id ,content: "kakarot")
+	end
+
+	test "user_id is presence" do 
+		@comment.user_id = ""
+		assert_not @comment.valid?
+	end
+
+	test "article_id is presence" do
+		@comment.article_id = ""
+		assert_not @comment.valid?
+	end
+
 end
