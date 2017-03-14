194ee4fcd8347676b127802b06d7eebd96483c13
yirolhao <yirolhao@gmail.com>
2017-03-14 11:31:18 +0800

    update view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
     
     @articles = @user.articles.paginate(page: params[:page], per_page: 10)
+    
   end
 
   def new
@@ -39,7 +40,7 @@ class UsersController < ApplicationController
 
   def articles
     @user = User.find(params[:id])
-    @articles = @user.articles.paginate(page: params[:page])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 10)
     render 'show_articles'
   end
 
