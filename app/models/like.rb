ba214fbbc2d1da91950935fc614587703bfe4bec
yirolhao <yirolhao@gmail.com>
2017-03-21 13:02:02 +0800

    格式

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,5 @@
 class Like < ApplicationRecord
+
 	belongs_to :liker, class_name: "User" #,touch: true
 	# after_touch do 
 	# 	puts "Liker belong to User"
@@ -7,7 +8,7 @@ class Like < ApplicationRecord
 	belongs_to :liked_article, class_name: "Article"
 	#liked_article must exit.
 	# ---  以后寻求解决办法 ---
-	
+
 	# belongs_to :liked_comment, class_name: "Comment"
 	validates :liker_id, presence: true
 	validates :liker_id, uniqueness: { scope: :liked_article_id, message: "唯一"}
