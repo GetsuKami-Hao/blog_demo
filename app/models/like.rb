e33d3c0aa680662639db1c23a4659e8929551f80
yirolhao <yirolhao@gmail.com>
2017-03-24 17:14:24 +0800

    进行格式化处理

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
 
   belongs_to :liker, class_name: "User"
 
-  #liked_article must exit.
+  # liked_article must exit.
   # ---  以后寻求解决办法 ---
   belongs_to :liked_article, class_name: "Article"
 
