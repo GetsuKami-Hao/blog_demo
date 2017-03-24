6ff941e2a9c5846c3ef063caf5b0ba48d5ed6610
yirolhao <yirolhao@gmail.com>
2017-03-24 15:25:12 +0800

    delete validators file

--- app/validators/email_validator.rb
+++
@@ -1,8 +0,0 @@
-class EmailValidator < ActiveModel::EachValidator
-
-  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-                      
-  def validate_each(record, attribute, value)
-    record.errors[attribute] << (options[:message] || 'is not a valid email') unless value =~ VALID_EMAIL_REGEX
-  end
-end
